553a6ff1a3c1bd9edfd4d9706d5ea8ff33837cb2
yani82 <yani.iman1@gmail.com>
2021-02-21 02:31:24 -0500

    commit tree 6aa9b1e13b2c98cd2012f681c49b4d48d136d4b7

--- main.js
+++ main.js
@@ -4,7 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+}; 
+
+let articleHearts = document.querySelectorAll(".like"); 
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "";
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
